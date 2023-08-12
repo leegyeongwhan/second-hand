10f2383b01bc5b071ad162819ab2a2a8da4d7f70
leegyeongwhan <leekhy02@naver.com>
2023-08-12 21:39:59 +0900

    ✨ feat: 레디스 발행 추가

---
+++ be/src/main/java/com/secondhand/domain/chat/RedisPublisher.java
@@ -0,0 +1,27 @@
+package com.secondhand.domain.chat;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Getter
+@RequiredArgsConstructor
+public class RedisPublisher {
+    private final ApplicationEventPublisher eventPublisher;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Transactional
+    public void publish(String topic, ChatMessage chatMessage) {
+        log.debug("pub log : " + chatMessage.toString() + "/ topic: " + topic);
+        //    chatMessage.ready();
+        redisTemplate.convertAndSend(topic, chatMessage);
+
+        eventPublisher.publishEvent(new ChatMessageEvent(chatMessage));
+    }
+}

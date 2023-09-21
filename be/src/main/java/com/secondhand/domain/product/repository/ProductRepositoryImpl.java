9aa7a65be680957062fdc517bbeeb4cb1e919b3e
leegyeongwhan <leekhy02@naver.com>
2023-09-22 08:43:59 +0900

    ✨ feat: 페이지 네이션 nooffset으로 변경
    만건 이상시 6초에서 -> 2초로 성능 개선

--- be/src/main/java/com/secondhand/domain/product/repository/ProductRepositoryImpl.java
+++ be/src/main/java/com/secondhand/domain/product/repository/ProductRepositoryImpl.java
@@ -1,26 +1,22 @@
 package com.secondhand.domain.product.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.secondhand.domain.interested.Interested;
-import com.secondhand.domain.interested.QInterested;
-import com.secondhand.domain.member.Member;
 import com.secondhand.domain.product.Product;
-import com.secondhand.domain.product.QProduct;
 import com.secondhand.domain.product.Status;
-import com.secondhand.service.MemberService;
-import com.secondhand.web.dto.filtercondition.ProductSalesSearchCondition;
 import com.secondhand.web.dto.filtercondition.ProductCategorySearchCondition;
+import com.secondhand.web.dto.filtercondition.ProductSalesSearchCondition;
 import com.secondhand.web.dto.filtercondition.ProductSearchCondition;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.secondhand.domain.categorie.QCategory.category;
 import static com.secondhand.domain.interested.QInterested.interested;
@@ -37,25 +33,57 @@ public class ProductRepositoryImpl implements ProductCustomRepository {
     // 페이지 크기를 10으로 고정
     public static final int PAGE_SIZE = 10;
     private final JPAQueryFactory jpaQueryFactory;
-    private final MemberService memberService;
+//
+//    /**
+//     * V1 offset paging
+//     * @param condition
+//     * @param pageable
+//     * @param userId
+//     * @return
+//     */
+//    @Override
+//    public Slice<Product> findAllByTowns(ProductSearchCondition condition, Pageable pageable, long userId) {
+//
+//        log.debug("querydsl 실행 ========================");
+//        JPAQuery<Product> query = jpaQueryFactory.selectFrom(product)
+//                .leftJoin(product.towns, town).fetchJoin()
+//                .leftJoin(product.category, category).fetchJoin()
+//                .leftJoin(product.member, member).fetchJoin()
+//                .where(locationEq(condition.getTownId()),
+//                        categoryEq(condition.getCategoryId())
+//                )
+//                .orderBy(product.id.desc());
+//
+//        log.debug("offset = {}", pageable.getPageNumber() * PAGE_SIZE);
+//        log.debug("pageable.getPageNumber() = {}", pageable.getPageNumber());
+//        List<Product> products = query.offset(pageable.getPageNumber() * PAGE_SIZE)
+//                .limit(PAGE_SIZE)
+//                .fetch();
+//
+//        log.debug("qurelydsl 종료 =================");
+//        int nextPageIndex = pageable.getPageNumber() * PAGE_SIZE;
+//        return new SliceImpl<>(products, pageable, hasNext(products, PAGE_SIZE + nextPageIndex));
+//    }
 
+    /**
+     * V2 Noffset paging
+     *
+     * @param condition
+     * @param pageable
+     * @param userId
+     * @return
+     */
     @Override
     public Slice<Product> findAllByTowns(ProductSearchCondition condition, Pageable pageable, long userId) {
-
+        int pageSize = pageable.getPageSize() + 1;
         log.debug("querydsl 실행 ========================");
-        JPAQuery<Product> query = jpaQueryFactory.selectFrom(product)
-                .leftJoin(product.towns, town).fetchJoin()
-                .leftJoin(product.category, category).fetchJoin()
-                .leftJoin(product.member, member).fetchJoin()
+
+        List<Product> products = jpaQueryFactory.selectFrom(product)
                 .where(locationEq(condition.getTownId()),
                         categoryEq(condition.getCategoryId())
                 )
-                .orderBy(product.id.desc());
-
-        log.debug("offset = {}", pageable.getPageNumber() * PAGE_SIZE);
-        log.debug("pageable.getPageNumber() = {}", pageable.getPageNumber());
-        List<Product> products = query.offset(pageable.getPageNumber() * PAGE_SIZE)
-                .limit(PAGE_SIZE)
+                .limit(pageSize)
+                .orderBy(product.id.desc())
                 .fetch();
 
         log.debug("qurelydsl 종료 =================");
@@ -129,14 +157,6 @@ public class ProductRepositoryImpl implements ProductCustomRepository {
         return product.status.eq(Status.SOLD);
     }
 
-    private BooleanExpression isStatusEq(Integer status) {
-        if (status == null) {
-            return null;  // status가 null 또는 비어있는 경우 조건을 적용하지 않음
-        }
-
-        return product.status.in(Status.getStatusByValue(status));
-    }
-
 
     private boolean hasNext(List<Product> fetch, int pageSize) {
         return fetch.size() > pageSize;
@@ -148,14 +168,6 @@ public class ProductRepositoryImpl implements ProductCustomRepository {
         }
         return product.towns.townId.eq(locationId);
     }
-
-    private BooleanExpression categoryListEq(Long categoryId, List<Long> interesteds) {
-        if (categoryId == null) {
-            return null;
-        }
-        return product.category.categoryId.in(interesteds);
-    }
-
 
     private BooleanExpression categoryEq(Long categoryId) {
         if (categoryId == null) {
--- be/src/main/java/com/secondhand/web/dto/filtercondition/ProductSearchCondition.java
+++ be/src/main/java/com/secondhand/web/dto/filtercondition/ProductSearchCondition.java
@@ -2,12 +2,14 @@ package com.secondhand.web.dto.filtercondition;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductSearchCondition {
-//    private final int pageNum;
+    //    private final int pageNum;
     private Long townId;
     private Long categoryId;
 }

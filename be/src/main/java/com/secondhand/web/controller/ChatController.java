31d3aceb2919104fbb1c7d1a23e5b63949fd1dc7
leegyeongwhan <leekhy02@naver.com>
2023-09-26 11:44:46 +0900

    ✨ feat: 기존의 Stomp기능을 레디스를 활용하도록 변경함에 따라 컨트롤러 코드 수정

--- be/src/main/java/com/secondhand/web/controller/ChatController.java
+++ be/src/main/java/com/secondhand/web/controller/ChatController.java
@@ -1,28 +1,33 @@
 package com.secondhand.web.controller;
 
-import com.secondhand.domain.chat.ChatMessage;
+import com.secondhand.domain.chat.dto.ChatMessage;
+import com.secondhand.domain.chat.repository.ChatRoomRepository;
 import com.secondhand.domain.chat.topic.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
 
 @Slf4j
-@RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/chats")
+@Controller
 public class ChatController {
 
     private final RedisPublisher redisPublisher;
-    private final ChannelTopic channelTopic;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/message")
-    public void chat(@Valid ChatMessage chatRequest) {
-        log.debug("pub controller");
-        redisPublisher.publish(channelTopic.getTopic(), chatRequest);
+    public void message(ChatMessage message) {
+        log.info("chat controller 호출");
+        System.out.println("message = " + message);
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }

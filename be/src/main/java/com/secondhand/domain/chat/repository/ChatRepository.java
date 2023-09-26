364160f0bc28176fb1197ad9d432963403455ae0
leegyeongwhan <leekhy02@naver.com>
2023-09-26 11:47:23 +0900

    ♻️  refactor : Stomp에서 레디스로 변경

--- be/src/main/java/com/secondhand/domain/chat/repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.secondhand.domain.chat.repository;
-
-import com.secondhand.domain.chat.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<ChatRoom, Long> {
-}

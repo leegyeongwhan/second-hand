26e21cc9f90d2f148aa5f2bd0368a4a119234344
leegyeongwhan <leekhy02@naver.com>
2023-09-26 11:43:20 +0900

    ✨ feat: 레디스 PUB/SUB 변경
    스프링부트에서 제공해주는 체널토픽을 사용한다.

--- be/src/main/java/com/secondhand/domain/chat/topic/RedisPublisher.java
+++ be/src/main/java/com/secondhand/domain/chat/topic/RedisPublisher.java
@@ -1,30 +1,23 @@
 package com.secondhand.domain.chat.topic;
 
-import com.secondhand.domain.chat.ChatMessage;
-import com.secondhand.domain.chat.ChatMessageEvent;
-import com.secondhand.domain.chat.dto.request.ChatRequest;
+import com.secondhand.domain.chat.dto.ChatMessage;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@Service
 @Getter
 @RequiredArgsConstructor
+@Service
 public class RedisPublisher {
-    private final ApplicationEventPublisher eventPublisher;
     private final RedisTemplate<String, Object> redisTemplate;
 
-    @Transactional
-    public void publish(String topic, ChatMessage chatMessage) {
-        log.debug("pub log : " + chatMessage.toString() + "/ topic: " + topic);
-        //    chatMessage.ready();
-        redisTemplate.convertAndSend(topic, chatMessage);
-
-        eventPublisher.publishEvent(new ChatMessageEvent(chatMessage));
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        System.out.println("topic = " + topic);
+        log.debug("pub log : " +  message.toString() + "/ topic: " + topic);
+        redisTemplate.convertAndSend(topic.getTopic(), message);
     }
 }

5c9e1b73134e1b7e5675c9e8a71b29301491e8e2
leegyeongwhan <leekhy02@naver.com>
2023-09-29 23:02:17 +0900

    ✨ feat: 채팅에 필요한 DTO추가

---
+++ be/src/main/java/com/secondhand/domain/chat/dto/request/ChatRequest.java
@@ -0,0 +1,10 @@
+package com.secondhand.domain.chat.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ChatRequest {
+    private final Long productId;
+}

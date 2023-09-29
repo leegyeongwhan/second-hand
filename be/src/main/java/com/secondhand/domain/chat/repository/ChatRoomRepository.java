dd738a7690fa53ea5d964d10c4d0368a4ee3e5f9
leegyeongwhan <leekhy02@naver.com>
2023-09-29 23:06:01 +0900

    ✨ feat: mysql에 저장할 chatroom 정보와 redis 인메모리에 저장할 정보 구분

--- be/src/main/java/com/secondhand/domain/chat/repository/ChatRoomRepository.java
+++ be/src/main/java/com/secondhand/domain/chat/repository/ChatRoomRepository.java
@@ -1,71 +1,8 @@
 package com.secondhand.domain.chat.repository;
 
 import com.secondhand.domain.chat.ChatRoom;
-import com.secondhand.domain.chat.topic.RedisSubscriber;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-@RequiredArgsConstructor
-@Repository
-public class ChatRoomRepository {
-    // 채팅방(topic)에 발행되는 메시지를 처리할 Listner
-    private final RedisMessageListenerContainer redisMessageListener;
-    // 구독 처리 서비스
-    private final RedisSubscriber redisSubscriber;
-    // Redis
-    private static final String CHAT_ROOMS = "CHAT_ROOM";
-    private final RedisTemplate<String, Object> redisTemplate;
-    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
-    // 채팅방의 대화 메시지를 발행하기 위한 redis topic 정보. 서버별로 채팅방에 매치되는 topic정보를 Map에 넣어 roomId로 찾을수 있도록 한다.
-    private Map<String, ChannelTopic> topics;
-
-    @PostConstruct
-    private void init() {
-        opsHashChatRoom = redisTemplate.opsForHash();
-        topics = new ConcurrentHashMap<>();
-    }
-
-    public List<ChatRoom> findAllRoom() {
-        return opsHashChatRoom.values(CHAT_ROOMS);
-    }
-
-    public ChatRoom findRoomById(String id) {
-        return opsHashChatRoom.get(CHAT_ROOMS, id);
-    }
-
-    /**
-     * 채팅방 생성 : 서버간 채팅방 공유를 위해 redis hash에 저장한다.
-     */
-    public ChatRoom createChatRoom(String name) {
-        ChatRoom chatRoom = ChatRoom.create(name);
-        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
-        return chatRoom;
-    }
-
-    /**
-     * 채팅방 입장 : redis에 topic을 만들고 pub/sub 통신을 하기 위해 리스너를 설정한다.
-     * DTO를 받아 하나의 토픽에 맞는 리스너를 생성해  STOMP 로 값을 전달해주면 매번 토픽을 리스너에 추가안해줘도 된다.
-     * 이럴경우 매번 리스너에 topic을 추가해주지 않아도된다.
-     * 하지만 다음과 같이 할경우 topic 하나당 방을 1:1로 매핑 시키는 것으로 설계했기 때문이다.
-     */
-    public void enterChatRoom(String roomId) {
-        topics.computeIfAbsent(roomId, key -> {
-            ChannelTopic topic = new ChannelTopic(key);
-            redisMessageListener.addMessageListener(redisSubscriber, topic);
-            return topic;
-        });
-    }
-
-    public ChannelTopic getTopic(String roomId) {
-        return topics.get(roomId);
-    }
 }

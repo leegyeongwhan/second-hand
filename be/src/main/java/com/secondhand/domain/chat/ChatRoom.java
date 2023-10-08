3af3e3cf9094b209dd13bfec403b33e285d0b47c
leegyeongwhan <leekhy02@naver.com>
2023-10-09 06:49:28 +0900

    ✨ feat: 채팅 이벤트 이벤트 pub/sub 내용수정
    pub/sub시 이벤트를 발행하는 코드수정

--- be/src/main/java/com/secondhand/domain/chat/ChatRoom.java
+++ be/src/main/java/com/secondhand/domain/chat/ChatRoom.java
@@ -10,7 +10,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -33,15 +36,14 @@ public class ChatRoom extends BaseTimeEntity {
     @JoinColumn(name = "customer_id")
     private Member customer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "seller_id")
-    private Member seller;
+    //채팅방 하나에 판매자한명 구독하는 사고자하는 사람은 여러명
+    private Long seller;
 
     @Enumerated(EnumType.STRING)
     private ChatroomStatus chatroomStatus;
 
     @Builder
-    public ChatRoom(Long id, UUID chatroomId, Product product, Member customer, Member seller, ChatroomStatus chatroomStatus) {
+    public ChatRoom(Long id, UUID chatroomId, Product product, Member customer, Long seller, ChatroomStatus chatroomStatus) {
         this.id = id;
         this.chatRoomId = chatroomId;
         this.product = product;
@@ -55,11 +57,22 @@ public class ChatRoom extends BaseTimeEntity {
                 .chatroomId(UUID.randomUUID())
                 .product(product)
                 .customer(buyer)
-                .seller(product.getMember())
+                .seller(product.getMember().getId())
                 .chatroomStatus(ChatroomStatus.FULL)
                 .build();
     }
 
     public void setUserCount(long userCount) {
+    }
+
+    public List<String> getChatroomMemberIds() {
+        Map<Long, Member> chatroomMembers = getChatroomMembers();
+        return chatroomMembers.values().stream()
+                .map(Member::getLoginName)
+                .collect(Collectors.toList());
+    }
+
+    private Map<Long, Member> getChatroomMembers() {
+        return Map.of(customer.getId(), customer, this.seller, this.product.getMember());
     }
 }
---
+++ be/src/main/java/com/secondhand/domain/chat/service/ChatroomInfo.java
@@ -0,0 +1,19 @@
+package com.secondhand.domain.chat.service;
+
+import com.secondhand.domain.chat.ChatRoom;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class ChatroomInfo {
+    private final String roomId;
+    private final List<String> members;
+
+    public static ChatroomInfo from(ChatRoom chatroom) {
+        return new ChatroomInfo(chatroom.getChatRoomId().toString(), chatroom.getChatroomMemberIds());
+    }
+}
+

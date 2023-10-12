bc96a74faffab93fa84ec8a7419272f12fb55bf9
leegyeongwhan <leekhy02@naver.com>
2023-10-12 17:07:02 +0900

    ✅ test : Product쿼리 서비스 통합테스트 클래스구현

---
+++ be/src/test/java/com/secondhand/service/ProductQueryServiceTest.java
@@ -0,0 +1,28 @@
+package com.secondhand.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductQueryServiceTest {
+
+    @Test
+    void getDetailPage() {
+    }
+
+    @Test
+    void isValidMinePage() {
+    }
+
+    @Test
+    void getProductList() {
+    }
+
+    @Test
+    void getLikeProductList() {
+    }
+
+    @Test
+    void findById() {
+    }
+}

10656e4a52906d6daf65734a7167b311c8b4671c
leegyeongwhan <leekhy02@naver.com>
2023-10-18 15:15:04 +0900

    ✅ test : 서비스레이어 테스트를 위한 계층 추가

---
+++ be/src/test/java/com/secondhand/application/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.secondhand.application;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@SpringBootTest
+public @interface ApplicationTest {
+}

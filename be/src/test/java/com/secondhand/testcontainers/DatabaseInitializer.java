bd2e193eaae72bebc67ac0f66b54265e56bcfe8a
leegyeongwhan <leekhy02@naver.com>
2023-10-23 15:56:48 +0900

    ✅ test : 데이터베이스 예외 추가

--- be/src/test/java/com/secondhand/testcontainers/DatabaseInitializer.java
+++ be/src/test/java/com/secondhand/testcontainers/DatabaseInitializer.java
@@ -1,5 +1,6 @@
 package com.secondhand.testcontainers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
---
+++ be/src/test/java/com/secondhand/testcontainers/DatabaseInitializerExtension.java
@@ -0,0 +1,16 @@
+package com.secondhand.testcontainers;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+public class DatabaseInitializerExtension implements AfterEachCallback {
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        DatabaseInitializer databaseInitializer = (DatabaseInitializer) SpringExtension.getApplicationContext(context)
+                .getBean("databaseInitializer");
+        databaseInitializer.truncateTables();
+    }
+}
+

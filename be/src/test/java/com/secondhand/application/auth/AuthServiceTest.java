0a87aedecf53ea7e83b497f4aa4caf5975eb3085
leegyeongwhan <leekhy02@naver.com>
2023-10-30 23:50:58 +0900

    ✅ test : 리프레쉬 토큰 테스트 코드 작성

--- be/src/test/java/com/secondhand/application/auth/AuthServiceTest.java
+++ be/src/test/java/com/secondhand/application/auth/AuthServiceTest.java
@@ -6,6 +6,7 @@ import com.secondhand.domain.member.Member;
 import com.secondhand.domain.member.MemberProfile;
 import com.secondhand.domain.memberToken.MemberToken;
 import com.secondhand.domain.oauth.OAuthProvider;
+import com.secondhand.exception.v2.UnAuthorizedException;
 import com.secondhand.service.AuthService;
 import com.secondhand.web.dto.login.UserProfile;
 import com.secondhand.web.dto.login.request.LoginRequest;
@@ -73,31 +74,43 @@ public class AuthServiceTest extends ApplicationTestSupport {
             );
         }
 
-//        @DisplayName("리프레시 토큰이 존재하는 사용자가 다시 로그인을 시도할 때 존재하는 리프레시 토큰을 삭제하고 새로운 리프레시 토큰을 저장한다.")
-//        @Test
-//        void givenLoginDataAndAlreadyHasRefreshToken_whenLogin_thenSuccess() {
-//            // given
-//            mockingOAuthInfo();
-//
-//            LoginRequest request = new LoginRequest("joy");
-//            supportRepository.save(Member.builder()
-//                    .email("joy@naver.com")
-//                    .loginId("joy")
-//                    .profileUrl("url")
-//                    .build());
-//            supportRepository.save(RefreshToken.builder()
-//                    .memberId(1L)
-//                    .token("token.token.token")
-//                    .build());
-//
-//            // when
-//            authService.login(OAuthProvider.NAVER, request, "code");
-//
-//            // then
-//            assertThat(supportRepository.findById(RefreshToken.class, 1L).get().getToken())
-//                    .isNotEqualTo("token.token.token");
-//        }
-//
+        @DisplayName("리프레시 토큰이 존재하는 사용자가 다시 로그인을 시도할 때 존재하는 리프레시 토큰을 삭제하고 새로운 리프레시 토큰을 저장한다.")
+        @Test
+        void givenLoginDataAndAlreadyHasRefreshToken_whenLogin_thenSuccess() {
+            // given
+            given(kakaoRequester.getToken(anyString()))
+                    .willReturn(new OauthTokenResponse("xxx.xxx.xxx", "scope", "bearer"));
+            given(kakaoRequester.getUserProfile(any(OauthTokenResponse.class)))
+                    .willReturn(UserProfile.builder()
+                            .email("gamja@naver.com")
+                            .profileUrl("url")
+                            .build());
+
+            MemberProfile profile = supportRepository.save(MemberProfile.builder()
+                    .memberEmail("gamja@naver.com")
+                    .build());
+
+            LoginRequest request = new LoginRequest("joy");
+            supportRepository.save(Member.builder()
+                    .loginName("gamja")
+                    .imgUrl("url")
+                    .oauthProvider("KAKAO")
+                    .memberProfile(profile)
+                    .build());
+
+            supportRepository.save(MemberToken.builder()
+                    .memberId(1L)
+                    .memberToken("token.token.token")
+                    .build());
+
+            // when
+            authService.login(OAuthProvider.KAKAO, request, "code");
+
+            // then
+            assertThat(supportRepository.findById(MemberToken.class, 1L).get().getMemberToken())
+                    .isNotEqualTo("token.token.token");
+        }
+
 //        @DisplayName("아이디는 존재하지만 해당 아이디의 이메일과 Naver 이메일 정보가 일치하지 않는 로그인 정보가 주어지면 예외를 던진다.")
 //        @Test
 //        void givenInvalidLoginData_whenLogin_thenThrowsException() {
